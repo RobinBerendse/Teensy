bd6aab45f6c1de8315935e3a378f3becc15e9ba7
Robin <r.berendse@outlook.com>
2020-11-04 10:04:05 +0100

    All code in 1 file

--- Analog1/Analog1.ino
+++ Analog1/Analog1.ino
@@ -1,18 +1,20 @@
-/*
- * Analog1: read an analog input and send to the monitor
- *
-*/
-void setup()
-{                
-  Serial.begin(115200);
+#include <Encoder.h> // include the encoder library
+
+Encoder enc(14, 15); // create an encoder object
+
+long current_value; // current read
+long previous_value = -999; // previous read
+
+void setup() {
+  Serial.begin(57600); // open serial port
 }
 
-int val;
-
-void loop()                     
-{
-  val = analogRead(A0);  // A0 is PIN_F0, or we could just say "14"
-  //Serial.print("analog 0 is: ");
-  Serial.println(val);
-  delay(20);
+void loop() {
+   
+  current_value = enc.read(); // read the value of the encoder
+  
+  if (current_value != previous_value) { // compare to previous reading
+    Serial.println(current_value); // if it's changed, print to serial port
+    previous_value = current_value; // store the reading for next comparison
+  }
 }
--- TeensyCode/TeensyCode.ino
+++ TeensyCode/TeensyCode.ino
@@ -1,18 +1,93 @@
-/*
- * Analog1: read an analog input and send to the monitor
- *
-*/
-void setup()
-{                
-  Serial.begin(115200);
+#include <Encoder.h> // include the encoder library
+
+//Distance
+#define trigPin 2
+#define echoPin 3
+long duration;
+int distance;
+
+//Gyro
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+MPU6050 accelgyro;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+#define OUTPUT_READABLE_ACCELGYRO
+#define LED_PIN 13
+bool blinkState = false;
+
+
+
+Encoder enc(14, 15); // create an encoder object
+
+long current_value; // current read
+long previous_value = -999; // previous read
+
+void setup() {
+  //Distance
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  //Gyro
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    
+  Serial.begin(57600); // open serial port
+
+
+  accelgyro.initialize();
+  pinMode(LED_PIN, OUTPUT);
 }
 
-int val;
+void loop() {
+   
+  current_value = enc.read(); // read the value of the encoder
+  
+  if (current_value != previous_value) { // compare to previous reading
+    Serial.println(current_value); // if it's changed, print to serial port
+    previous_value = current_value; // store the reading for next comparison
+  }
 
-void loop()                     
-{
-  val = analogRead(A0);  // A0 is PIN_F0, or we could just say "14"
-  //Serial.print("analog 0 is: ");
-  Serial.println(val);
-  delay(20);
+  //Distance
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+
+  //Gyro
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+ 
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
+    #endif
+ 
+    
+ 
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+
+
+    delay(50);
 }
